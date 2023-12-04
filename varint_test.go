32b2dbe417872153c6e0bb18f24b2e7928f6ec64
John Pritchard <logicalexistentialism@gmail.com>
2023-12-04 18:06:42 -0500

    test

--- varint_test.go
+++ varint_test.go
@@ -127,34 +127,9 @@ func TestGoVintGet(t *testing.T){
 	}
 }
 /*
- * OFFLINE TEST
- * 
- *    --- FAIL: TestGoVintPut (0.00s)
- *  panic: runtime error: index out of range [8] with length 8 [recovered]
- * 	 panic: runtime error: index out of range [8] with length 8
- * 
- *  goroutine 22 [running]:
- *  testing.tRunner.func1.2({0x521dc0, 0xc0000cc000})
- * 	 /usr/local/src/go-1.20.11/src/testing/testing.go:1526 +0x24e
- *  testing.tRunner.func1()
- * 	 /usr/local/src/go-1.20.11/src/testing/testing.go:1529 +0x39f
- *  panic({0x521dc0, 0xc0000cc000})
- * 	 /usr/local/src/go-1.20.11/src/runtime/panic.go:884 +0x213
- *  encoding/binary.PutUvarint(...)
- * 	 /usr/local/src/go-1.20.11/src/encoding/binary/varint.go:58
- *  github.com/syntelos/go-varint.TestGoVintPut(0xc00009aea0)
- * 	 /home/jdp/go/src/github.com/syntelos/go-varint/varint_test.go:145 +0x2e5
- *  testing.tRunner(0xc00009aea0, 0x539598)
- * 	 /usr/local/src/go-1.20.11/src/testing/testing.go:1576 +0x10b
- *  created by testing.(*T).Run
- * 	 /usr/local/src/go-1.20.11/src/testing/testing.go:1629 +0x3ea
- *  exit status 2
- *  FAIL	github.com/syntelos/go-varint	0.006s
- * 
- *  Compilation exited abnormally with code 1 at Mon Dec  4 17:56:45
- * 
+ * GOPL consistency
  */
-func _TestGoVintPut(t *testing.T){
+func TestGoVintPut(t *testing.T){
 	var sequence uint8
 	for sequence = 1; sequence <= 16; sequence++ {
 
@@ -165,7 +140,7 @@ func _TestGoVintPut(t *testing.T){
 		if nil != er {
 			t.Error(er)
 		} else {
-			var bary []byte = make([]byte,8)
+			var bary []byte = make([]byte,10)
 
 			vz := binary.PutUvarint(bary,value)
 			if 1 > vz {
